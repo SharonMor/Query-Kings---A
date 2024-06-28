2d5efe45180819679c787662d3ff8d47192c7561
[SharonMor] <[sharonmor5869@gmail.com]>
2024-06-28 18:56:32 +0300

    add-css

---
+++ js/index.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    $('.carousel').slick({
+        autoplay: true,
+        autoplaySpeed: 1000,
+        dots: true,
+        arrows: true,
+        pauseOnHover: true,
+    });
+});
